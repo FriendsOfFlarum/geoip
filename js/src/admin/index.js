733b8e14fedc738ac6c6f7d8aa2a0f32e1e67543
Ian Morland <ian@morland.me>
2022-07-01 13:26:19 +0100

    prettier

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,5 +2,5 @@ import app from 'flarum/admin/app';
 import GeoipSettingsPage from './components/ExtensionSettingsPage';
 
 app.initializers.add('fof/geoip', () => {
-    app.extensionData.for('fof-geoip').registerPage(GeoipSettingsPage);
+  app.extensionData.for('fof-geoip').registerPage(GeoipSettingsPage);
 });
