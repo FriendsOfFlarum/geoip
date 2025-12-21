6e306226d01ed2a24ddcdd4b5f0f738df6cdb9e3
IanM <16573496+imorland@users.noreply.github.com>
2025-12-21 11:02:05 +0000

    chore: make settings searchable (#65)

--- js/src/admin/extend.ts
+++ js/src/admin/extend.ts
@@ -1,5 +1,21 @@
+import app from 'flarum/admin/app';
 import Extend from 'flarum/common/extenders';
 
 import { default as commonExtend } from '../common/extend';
+import GeoipSettingsPage from './components/GeoipSettingsPage';
 
-export default [...commonExtend];
+export default [
+  ...commonExtend,
+
+  new Extend.Admin() //
+    .page(GeoipSettingsPage)
+    .permission(
+      () => ({
+        icon: 'fas fa-globe',
+        permission: 'fof-geoip.canSeeCountry',
+        label: app.translator.trans('fof-geoip.admin.permissions.see_country'),
+      }),
+      'moderate',
+      50
+    ),
+];
